8014ef5e33a1559fd93204937d32bbd27e349c2b
Joe Brinkman <joe@dnncorp.com>
2013-04-17 16:37:49 -0400

    Update the final issue tracker link for the DotNetNuke tab extension.

--- Tab/DotNetNukeTab.cs
+++ Tab/DotNetNukeTab.cs
@@ -14,7 +14,7 @@ namespace DotNetNuke.Modules.dnnGlimpse.Tab
         {
             var plugin = Plugin.Create("Feedback");
 
-            var instructions = @"<h2>Using Glimpse with DotNetNuke</h2><div>This module is still under construction.  If you have DotNetNuke specific data that would be helpful to see here, then create an item in the <a href='http://machinemonitor.codeplex.com/workitem/list/basic'>issue tracker on CodePlex</a>.</div>";
+            var instructions = @"<h2>Using Glimpse with DotNetNuke</h2><div>This module is still under construction.  If you have DotNetNuke specific data that would be helpful to see here, then create an item in the <a href='https://dnnglimpse.codeplex.com/workitem/list/basic'>issue tracker on CodePlex</a>.</div>";
 
             plugin.AddRow()
                 .Column(instructions).Raw();
