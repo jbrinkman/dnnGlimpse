5c60070321312f4079cbf548c706fc79af88fadc
Joe Brinkman <joe@dnncorp.com>
2013-04-26 10:00:29 -0700

    Added security policy to prevent unathorized access to glimpse

---
+++ Libraries/DotNetNuke.Extensions.Glimpse/Builds/net4/GlimpseSecurityPolicy.cs
@@ -0,0 +1,20 @@
+﻿using Glimpse.AspNet.Extensions;
+using Glimpse.Core.Extensibility;
+ 
+using DotNetNuke.Entities.Users;
+
+namespace Glimpse.DotNetNuke
+{
+    public class GlimpseSecurityPolicy:IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            return UserController.GetCurrentUserInfo().IsSuperUser ? RuntimePolicy.On : RuntimePolicy.Off;
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+    }
+}
---
+++ Modules/dnnGlimpse/GlimpseSecurityPolicy.cs
@@ -0,0 +1,20 @@
+﻿using Glimpse.AspNet.Extensions;
+using Glimpse.Core.Extensibility;
+ 
+using DotNetNuke.Entities.Users;
+
+namespace DotNetNuke.Modules.dnnGlimpse
+{
+    public class GlimpseSecurityPolicy:IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            return UserController.GetCurrentUserInfo().IsSuperUser ? RuntimePolicy.On : RuntimePolicy.Off;
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+    }
+}
