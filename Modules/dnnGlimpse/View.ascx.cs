1d89c78497b575ad0615af6cac4528613c20f067
Brian Dukes <bdukes@engagesoftware.com>
2013-04-26 20:06:30 -0500

    Remove unused template cruft

--- Modules/dnnGlimpse/View.ascx.cs
+++ Modules/dnnGlimpse/View.ascx.cs
@@ -1,24 +1,20 @@
-﻿/*
-' Copyright (c) 2013  Christoc.com
-'  All rights reserved.
-' 
-' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-' DEALINGS IN THE SOFTWARE.
-' 
-*/
+﻿// <copyright file="View.ascx.cs" company="DotNetNuke Corp.">
+// dnnGlimpse
+// Copyright (c) 2013
+// by DotNetNuke Corp.
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 using System;
-using System.Web.UI.WebControls;
-using DotNetNuke.Modules.dnnGlimpse.Components;
+
 using DotNetNuke.Security;
-using DotNetNuke.Services.Exceptions;
 using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Modules.Actions;
 using DotNetNuke.Services.Localization;
-using DotNetNuke.UI.Utilities;
 
 namespace DotNetNuke.Modules.dnnGlimpse
 {
@@ -35,20 +31,8 @@ namespace DotNetNuke.Modules.dnnGlimpse
     /// 
     /// </summary>
     /// -----------------------------------------------------------------------------
-    public partial class View : dnnGlimpseModuleBase, IActionable
+    public partial class View : PortalModuleBase, IActionable
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            try
-            {
-
-            }
-            catch (Exception exc) //Module failed to load
-            {
-                Exceptions.ProcessModuleLoadException(this, exc);
-            }
-        }
-
         public ModuleActionCollection ModuleActions
         {
             get
