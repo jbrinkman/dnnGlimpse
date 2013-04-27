b6a13e8c44e4ad5540e120a8be7084c9de30ddb5
Brian Dukes <bdukes@engagesoftware.com>
2013-04-26 19:59:32 -0500

    Clean up JSHint warnings in jquery.glimpse.js

--- Modules/dnnGlimpse/Scripts/jquery.glimpse.js
+++ Modules/dnnGlimpse/Scripts/jquery.glimpse.js
@@ -1,24 +1,24 @@
-﻿jQuery(function ($) {
-    var
-    cookieName = 'glimpsePolicy',
-    $glimpse = $('#glimpseControl'),
-    glimpseCookie = dnn.dom.getCookie(cookieName);
+﻿/*global jQuery, dnn */
+jQuery(function ($) {
+    'use strict';
+    var cookieUtility = dnn.dom,
+        cookieName = 'glimpsePolicy',
+        $glimpse = $('#glimpseControl'),
+        glimpseCookie = cookieUtility.getCookie(cookieName);
     if (glimpseCookie) {
         $glimpse.text('Disable Glimpse');
-    }
-    else {
+    } else {
         $glimpse.text('Enable Glimpse');
-    };
+    }
 
     $glimpse.click(function (e) {
         e.preventDefault();
         if (glimpseCookie) {
             //document.cookie = 'glimpsePolicy=; path=/; expires=Sat, 01 Jan 2050 12:00:00 GMT;';
-            dnn.dom.deleteCookie(cookieName, '/');
+            cookieUtility.deleteCookie(cookieName, '/');
+        } else {
+            cookieUtility.setCookie(cookieName, 'On', 30, '/');
         }
-        else {
-            dnn.dom.setCookie(cookieName, 'On', 30, '/')
-        };
         window.location.reload();
     });
 });
