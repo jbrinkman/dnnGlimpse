d4a9f08e401bcc1292f2e46e5450656ac545aab2
Joe Brinkman <joe@dnncorp.com>
2013-05-02 19:14:18 -0400

    Replace the On/Off button with Toggle Button.  This will be default UI for all checkboxes in the setting screen.

--- Modules/dnnGlimpse/Scripts/jquery.glimpse.js
+++ Modules/dnnGlimpse/Scripts/jquery.glimpse.js
@@ -1,24 +1,25 @@
 ﻿/*global jQuery, dnn */
-jQuery(function ($) {
+
+/* code should execute at bottom of doc to ensure dom has loaded */
+(function ($) {
     'use strict';
     var cookieUtility = dnn.dom,
         cookieName = 'glimpsePolicy',
-        $glimpse = $('#glimpseControl'),
-        glimpseCookie = cookieUtility.getCookie(cookieName);
-    if (glimpseCookie) {
-        $glimpse.text('Disable Glimpse');
-    } else {
-        $glimpse.text('Enable Glimpse');
-    }
+        $glimpse = $('#enableGlimpse'),
+        enabled = cookieUtility.getCookie(cookieName) == 'On';
+
+    $glimpse.prop('checked', enabled);
 
     $glimpse.click(function (e) {
         e.preventDefault();
-        if (glimpseCookie) {
+        if (enabled) {
             //document.cookie = 'glimpsePolicy=; path=/; expires=Sat, 01 Jan 2050 12:00:00 GMT;';
             cookieUtility.deleteCookie(cookieName, '/');
+            $glimpse.prop('checked', !(enabled));
         } else {
             cookieUtility.setCookie(cookieName, 'On', 30, '/');
+            $glimpse.prop('checked', !(enabled));
         }
         window.location.reload();
     });
-});
+})(jQuery);
--- Modules/dnnGlimpse/View.ascx
+++ Modules/dnnGlimpse/View.ascx
@@ -1,6 +1,8 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="DotNetNuke.Modules.dnnGlimpse.View" %>
 <%@ Register TagPrefix="dnn" Namespace="DotNetNuke.Web.Client.ClientResourceManagement" Assembly="DotNetNuke.Web.Client" %>
 
-<dnn:DnnJsInclude ID="glimpseInclude" runat="server" FilePath="~/Desktopmodules/dnnGlimpse/Scripts/jquery.glimpse.js" Priority="20" />
+<dnn:DnnJsInclude ID="glimpseInclude" runat="server" FilePath="~/Desktopmodules/dnnGlimpse/Scripts/jquery.glimpse.js" ForceProvider="DnnFormBottomProvider" />
 
-<a id="glimpseControl" class="dnnPrimaryAction" href=".">Enable Glimpse</a>
+<div class="iphone-toggle-buttons">
+    <div><label for="enableGlimpse">Enable Glimpse</label><input type="checkbox" name="enableGlimpse" id="enableGlimpse" /><span></span></div>
+</div>
