0cd65336d1eeb318a206fecc431361f405d3db02
Brian Dukes <bdukes@engagesoftware.com>
2014-02-04 20:41:53 -0600

    Update Glimpse to 1.8.2, bump dnnGlimpse to 1.0.5

--- Modules/dnnGlimpse/Properties/AssemblyInfo.cs
+++ Modules/dnnGlimpse/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("01.00.04.*")]
-[assembly: AssemblyFileVersion("01.00.04")]
+[assembly: AssemblyVersion("01.00.05.*")]
+[assembly: AssemblyFileVersion("01.00.05")]
