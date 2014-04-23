52ae2928e57ca65fe0024eaf87127f4e43dd84c3
Brian Dukes <bdukes@engagesoftware.com>
2014-04-23 14:17:04 -0500

    Bump version to 1.0.8

--- Modules/dnnGlimpse/Properties/AssemblyInfo.cs
+++ Modules/dnnGlimpse/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("01.00.07.*")]
-[assembly: AssemblyFileVersion("01.00.07")]
+[assembly: AssemblyVersion("01.00.08.*")]
+[assembly: AssemblyFileVersion("01.00.08")]
